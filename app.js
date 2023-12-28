c1439a5bbe1aae5f3f8262895ca6691778083560
Volodymyr <vladikon70@gmail.com>
2023-12-28 09:59:48 +0100

    change path auth

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use("/api/users", authRouter);
+app.use("/api/auth", authRouter);
 // app.use("/api/contacts", contactsRouter);
 app.use("/api/waternotes", waterRouter);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
